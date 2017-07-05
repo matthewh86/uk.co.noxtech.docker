6117967adc8be2b06fd0f791757c540426eb4cf1
Matthew Hayward <matthew.hayward@cdl.co.uk>
2017-07-05 08:45:31 +0100

    Add gitPull.sh convenience script

---
+++ gitPull.sh
@@ -0,0 +1,11 @@
+git pull
+cd uk.co.noxtech.docker.data
+git pull
+cd ../uk.co.noxtech.docker.consumer
+git pull
+cd ../uk.co.noxtech.docker.producer
+git pull
+cd ../uk.co.noxtech.docker.rabbitmq
+git pull
+cd ..
+
